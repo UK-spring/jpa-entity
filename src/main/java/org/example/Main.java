5e649dd817f2123c8b39aa3752114f49d9219f5a
UK <hwangwonuk94@gmail.com>
2024-11-08 04:00:28 +0900

    feat: 객체와 테이블 매핑

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,30 @@
 package org.example;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+import org.entity.Tutor;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("test");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction transaction = em.getTransaction();
+
+        transaction.begin();
+
+        try {
+
+            Tutor tutor = new Tutor(1L, "wonuk");
+            em.persist(tutor);
+
+            transaction.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            transaction.rollback();
+        } finally {
+            em.close();
+        }
     }
 }
